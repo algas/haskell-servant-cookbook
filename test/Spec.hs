5c0b360ad55af76758806cc994c9c277f80bef3a
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2016-05-28 12:21:13 +0900

    Update testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Main (main) where
+
+import           SimpleApi           (app)
+import           Test.Hspec
+import           Test.Hspec.Wai
+import           Test.Hspec.Wai.JSON
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = with (return app) $ do
+    describe "GET /" $ do
+        it "responds with 200" $ do
+            get "/" `shouldRespondWith` 200
+        it "responds with 'Simple'" $ do
+            get "/" `shouldRespondWith` "Simple"
